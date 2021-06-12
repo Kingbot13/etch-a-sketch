9bb334195bafe64700de000a3cf8313423e1d8b6
Dylan King <dwkblue51@yahoo.com>
2021-06-11 20:39:59 -0500

    add 32 div squares with javascript

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 /* 
+    for loop
+        create 16x16 grid of divs    
+    add class to divs
     
 */
 
 
 
 const container = document.querySelector(".container");  // container div
+// let gridItems;
+
+for (let i = 0; i < 32; i++){
+    let gridItems = document.createElement("div");
+    
+    container.appendChild(gridItems);
+
+};
+
