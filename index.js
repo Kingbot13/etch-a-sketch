3b182be675d82d8134fa519153009efb4daa08fa
Dylan King <dwkblue51@yahoo.com>
2021-06-13 20:28:41 -0500

    add button to reset grid

--- index.js
+++ index.js
@@ -5,22 +5,34 @@
     
 */
 
+const button = document.querySelector("button");
 
+function createGrid (){
 
-const container = document.querySelector(".container");  // container div
-let gridItems;
-
-for (let i = 0; i < 256; i++){
-    gridItems = document.createElement("div");
-    gridItems.classList.add("grid-item");
-    container.appendChild(gridItems);
-
-};
-
-const gridList = document.querySelectorAll(".grid-item");
-
-gridList.forEach(gridItems => {
-    gridItems.addEventListener("mouseenter", () => {
-        gridItems.classList.add("active");
+    
+    const container = document.querySelector(".container");  // container div
+    let gridItems;
+    
+    for (let i = 0; i < 256; i++){
+        gridItems = document.createElement("div");
+        gridItems.classList.add("grid-item");
+        container.appendChild(gridItems);
+        
+    };
+    
+    const gridList = document.querySelectorAll(".grid-item");
+    
+    gridList.forEach(gridItems => {
+        gridItems.addEventListener("mouseenter", () => {
+            gridItems.classList.add("active");
+        });
     });
+}
+
+button.addEventListener("click", () => {
+    a = prompt("What size grid?");
+    gridSize = parseInt(a);
+    createGrid()
+    
 });
+
