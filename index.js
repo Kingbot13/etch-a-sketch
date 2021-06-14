fdd2974a7aa63bcb573551b8ecb9acdbf20b5ebc
Dylan King <dwkblue51@yahoo.com>
2021-06-14 12:53:47 -0500

    reset grid each time button is pressed

--- index.js
+++ index.js
@@ -6,20 +6,23 @@
 */
 
 const button = document.querySelector("button");
+const container = document.querySelector(".container");  // container div
+
+// function refresh(){
+//     container.style.cssText = `grid-template-columns: repeat( 0, 1fr); grid-template-rows: repeat(0 1fr);`
+// };
 
 function createGrid (gridColumns){
 
     gridSize = gridColumns * gridColumns;
 
-    const container = document.querySelector(".container");  // container div
-    container.style.cssText += `grid-template-columns: repeat(${gridColumns}, 1fr); grid-template-rows: repeat(${gridColumns});`
+    container.style.cssText = `grid-template-columns: repeat(${gridColumns}, 1fr); grid-template-rows: repeat(${gridColumns}, 1fr);`
     let gridItems;
     
     for (let i = 0; i < gridSize; i++){
         gridItems = document.createElement("div");
         gridItems.classList.add("grid-item");
-        container.appendChild(gridItems);
-        
+        container.appendChild(gridItems); 
     };
     
     const gridList = document.querySelectorAll(".grid-item");
@@ -32,6 +35,7 @@ function createGrid (gridColumns){
 }
 
 button.addEventListener("click", () => {
+    // refresh();
     a = prompt("What size grid?");
     gridColumns = parseInt(a);
 
